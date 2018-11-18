deafc8ef22553489e586343867e31fbee38d25ed
noerrornowork <13058417431@163.com>
2018-11-18 10:23:51 +0800

    second commit

---
+++ mock/server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+http.createServer(function(req, res){
+
+}).listen(3000)
+
+console.log('Your application is running at port 3000......')
