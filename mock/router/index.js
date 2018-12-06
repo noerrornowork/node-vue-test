24f37a488c88ac30a069ffa34d09138719c14810
noerrornowork <13058417431@163.com>
2018-12-06 13:05:39 +0800

    kk

--- mock/router/index.js
+++ mock/router/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const url = require('url')
+const mongoose = require('mongoose')
 const db = require('../model/db.js')
 
 const router = express.Router()
+
+let pageSize = 5
 
 router.get('/', (req, res) => {
 	res.render('index.html')
@@ -29,8 +32,10 @@ router.get('/hot', (req, res) => {
 router.use('/book', async (req, res, next) => {
 		// 解析url路径
 	let {pathname, query} = url.parse(req.url, true)
-
 	let id = query.id
+	// let id = mongoose.Types.ObjectId(query.id)
+	console.log(id)
+	console.log(typeof id)
 	
 	switch (req.method) {
 		case 'GET':
@@ -60,17 +65,17 @@ router.use('/book', async (req, res, next) => {
 			})
 			break
 		case 'PUT':
-			if (id) {
+				id = mongoose.Types.ObjectId(query.id)
 				await db.book.findByIdAndUpdate(id, req.body, (err, data) => {
 					if (err) {
 						return res.status(500).send('服务器忙,请稍后重试')
 					}
 					res.send('ok')
 				})
-			}
 			break
 		case 'DELETE':
-			await db.book.remove({_id: id}, (err, data) => {
+		  id = mongoose.Types.ObjectId(query.id)
+			await db.book.deleteById(id, (err, data) => {
 				if (err) {
 					return res.status(500).send('服务器忙,请稍后重试')
 				}
@@ -80,5 +85,24 @@ router.use('/book', async (req, res, next) => {
 	}
 	return
  })
+
+router.get('/page', async (req, res, next) => {
+
+	let {pathname, query} = url.parse(req.url, true)
+
+	let offset = parseInt(query.offset) || 0
+
+	await db.book.find((err, data) => {
+		if (err) {
+			return res.status(500).send('服务器忙,请稍后重试')
+		}
+		let hasMore = true
+		if (data.length <= offset + pageSize) {
+			hasMore = false
+		}
+		let results = data.reverse().slice(offset, offset + pageSize)
+		res.send({hasMore, books: results})
+	})
+})
 
 module.exports = router
